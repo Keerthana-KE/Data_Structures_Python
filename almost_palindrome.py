ab3aae743c7ef76e0235d3eb2e709a0a015207dc
Keerthana-KE <keerthanakbose@gmail.com>
2026-07-20 23:39:41 +0530

    added

---
+++ almost_palindrome.py
@@ -0,0 +1,15 @@
+def almost_palindrome(s):
+    for i in range(len(s)):
+        str=s[:i] + s[i+1:]
+        if(str==str[::-1]):
+            return True
+    return False
+
+s=input("enter a string:")
+s=s.replace(" ","").lower()
+if(s==s[::-1]):
+    print("palindrome")
+elif(almost_palindrome(s)):
+    print("almost palindrome")
+else:
+    print("not a palindrome")
